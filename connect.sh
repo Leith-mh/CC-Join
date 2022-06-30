3f0b78cc63953239e6df5c3a1e64b1482a3dc81e
Leith-mh <76526645+Leith-mh@users.noreply.github.com>
2022-07-01 00:25:44 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -17,7 +17,3 @@ systemctl enable netclient
 netclient join -t eyJhcGljb25uc3RyaW5nIjoiYXBpLm5tLjU0LTIzNy0zOC0xMzUubmlwLmlvOjQ0MyIsIm5ldHdvcmsiOiJrM3MiLCJrZXkiOiJmZDk2ZTUwMGVmMzA5NDhjIiwibG9jYWxyYW5nZSI6IiJ9
 
 
-# Install K3S and connect to the control plane
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.21.7+k3s1 K3S_URL=https://172.30.16.1:6443 K3S_TOKEN=K10ece84e5bc0ffa0759311c80e6eaba2db7e486a745bd02f133f01838b3dd5a3ea::server:823348c08eeeafa03058f0fe9294ef9c |
-sh -
-
