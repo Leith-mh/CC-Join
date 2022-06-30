26c6e13b6c1faa6b1c9e8a8568cd2b0c95cded13
Leith-mh <76526645+Leith-mh@users.noreply.github.com>
2022-06-30 23:18:43 +0100

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+# Install wireguard
+apt install wireguard-tools
+
+# install netclient
+curl -sL 'https://apt.netmaker.org/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/n>
+curl -sL 'https://apt.netmaker.org/debian.deb.txt' | sudo tee /etc/apt/sources.>
+sudo apt update
+sudo apt install netclient
+systemctl enable netclient
+
+# Join the node to the mesh network
+netclient join -t eyJhcGljb25uc3RyaW5nIjoiYXBpLm5tLjU0LTIzNy0zOC0xMzUubmlwLmlvOjQ0MyIsIm5ldHdvcmsiOiJrM3MiLCJrZXkiOiJmZDk2ZTUwMGVmMzA5NDhjIiwibG9jYWxyYW5nZSI6IiJ9
+
+
+# Install K3S and connect to the control plane
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.21.7+k3s1 K3S_URL=https://172.30.16.1:6443 K3S_TOKEN=K10ece84e5bc0ffa0759311c80e6eaba2db7e486a745bd02f133f01838b3dd5a3ea::serve>
+ sh -
+
